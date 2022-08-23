2960f515c735f095b32c09bf23bd43eed82ca3df
Xlenco <75837117+xlenco@users.noreply.github.com>
2022-08-23 12:31:26 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: 'e1553839e22942abaca2d7243b480ae8',
+  rootNotionPageId: 'd785f81ea8c942f5a769e68c00548c75',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
